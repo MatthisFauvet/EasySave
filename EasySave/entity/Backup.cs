8c2b5b9ce3d4c06bd1c1aa8d38594d84c4bc46a5
matth <matthisf.etudes@gmail.com>
2026-02-04 14:43:24 +0100

    :building_construction: Created interface file help devs to code together

---
+++ EasySave/entity/Backup.cs
@@ -0,0 +1,10 @@
+﻿namespace EasySave.Entity;
+
+public class Backup
+{
+    private int _id;
+    private string _name;
+    private string _sourceFilePath;
+    private string _destinationFilePath;
+    private string _timeStamp;
+}
---
+++ EasySave/service/IBackupService.cs
@@ -0,0 +1,14 @@
+﻿using EasyLog.writers;
+
+namespace EasySave.service;
+
+public interface IBackupService
+{
+    
+    /// <summary>
+    /// This method execute a save method on all created backup
+    /// </summary>
+    /// <param name="backupIds">List of ids for the backups you want to execute</param>
+    /// <returns>Return false if issues happened else true</returns>
+    public bool ExecuteBackup(List<int> backupIds);
+}
