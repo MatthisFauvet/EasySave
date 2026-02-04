6ca400d9eff774e515cc2ba071990301e4e501b1
matth <matthisf.etudes@gmail.com>
2026-02-04 14:39:27 +0100

    :tada: Created C# project EasySave

---
+++ EasySave/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using EasyLog;
+using EasyLog.entity;
+
+Console.WriteLine("Hello, World!");
+
+Logger logger = new Logger();
+logger.InitWriters();
+logger.Log("Hello, World!", LogType.Error);
+logger.Log("Hello, World!", LogType.Warning);
+logger.Log("Hello, World!", LogType.Info);
