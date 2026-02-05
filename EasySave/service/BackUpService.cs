47ceeaf885457df5cd911b7de231457ad8a19215
Fenda78 <fcisse@tf1.fr>
2026-02-05 16:21:48 +0100

    :ambulance: Résolution des conflits

--- EasySave/service/BackUpService.cs
+++ EasySave/service/BackUpService.cs
@@ -49,10 +49,11 @@ public class BackupService : IBackupService
         return isSuccessful;
     }
 
-    private void ExecuteSingleBackup(Backup backup)
+    private void ExecuteSingleBackup(int backupId)
     {
         Logger _logger = new Logger();
         _logger.InitWriters(backup.DestinationFilePath, $"Execution du backup {backup.Id}");
+
 
         if (!Directory.Exists(backup.SourceFilePath)) {
             _logger.Log($"Source directory not found : {backup.SourceFilePath}", LogType.Error);
