588335dda7913bd31f02cd138375dbb1e97a58dd
matth <matthisf.etudes@gmail.com>
2026-02-11 14:58:03 +0100

    :building_construction: Created data type (model / entity) and interfaces

---
+++ EasySave/EasySave/Model/Backup.cs
@@ -0,0 +1,34 @@
+﻿namespace EasySave.Model;
+
+public class Backup
+{
+    public Backup()
+    {
+    }
+
+    public Backup(int id, string name, string sourceFilePath, string destinationFilePath, DateTime lastBackupDateTime, BackupType type)
+    {
+        Id = id;
+        Name = name;
+        SourceFilePath = sourceFilePath;
+        DestinationFilePath = destinationFilePath;
+        LastBackupDateTime = lastBackupDateTime;
+        Type = type;
+    }
+
+    public Backup(int id, string name, string sourceFilePath, string destinationFilePath, string type)
+    { 
+        Id = id;
+        Name = name;
+        SourceFilePath = sourceFilePath;
+        DestinationFilePath = destinationFilePath;
+        Type = BackupTypeHelper.FromString(type);
+    }
+
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string SourceFilePath { get; set; }
+    public string DestinationFilePath { get; set; }
+    public DateTime LastBackupDateTime { get; set; }
+    public BackupType Type { get; set; }
+}
---
+++ EasySave/EasySave/Model/BackupCreateRequest.cs
@@ -0,0 +1,16 @@
+﻿namespace EasySave.Model;
+
+public class BackupCreateRequest
+{
+    public BackupCreateRequest(string name, string sourceFilePath, string destinationFilePath, BackupType type)
+    {
+        Name = name;
+        SourceFilePath = sourceFilePath;
+        DestinationFilePath = destinationFilePath;
+        Type = type;
+    }
+    public string Name { get; set; }
+    public string SourceFilePath { get; set; }
+    public string DestinationFilePath { get; set; }
+    public BackupType Type { get; set; }
+};
